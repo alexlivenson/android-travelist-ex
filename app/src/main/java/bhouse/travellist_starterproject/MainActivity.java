30fe5ccbc444e911400403cd10d6c7b48de7f5d5
alex livenson <alexlivenson1@alexs-MacBook-Pro.local>
2016-02-09 00:05:06 -0800

    Added clicking action / next page

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -1,6 +1,7 @@
 package bhouse.travellist_starterproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -32,6 +33,18 @@ public class MainActivity extends Activity {
 
         mAdapter = new TravelListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+
+        TravelListAdapter.OnItemClickListener onItemClickListener = new TravelListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+                startActivity(intent);
+            }
+        };
+
+        mAdapter.setOnItemClickListener(onItemClickListener);
     }
 
     private void setUpActionBar() {
@@ -60,10 +73,12 @@ public class MainActivity extends Activity {
     private void toggle() {
         MenuItem item = menu.findItem(R.id.action_toggle);
         if (isListView) {
+            mStaggeredLayoutManager.setSpanCount(2);
             item.setIcon(R.drawable.ic_action_list);
             item.setTitle("Show as list");
             isListView = false;
         } else {
+            mStaggeredLayoutManager.setSpanCount(1);
             item.setIcon(R.drawable.ic_action_grid);
             item.setTitle("Show as grid");
             isListView = true;
--- app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
+++ app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -1,6 +1,9 @@
 package bhouse.travellist_starterproject;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,9 +77,25 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
 
     // 3. binds the Place object to the UI element in ViewHolder. You'll use Picasso to Cache those images
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         final Place place = new PlaceData().placeList().get(position);
         holder.placeName.setText(place.name);
         Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+
+        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), place.getImageResourceId(mContext));
+        /*
+            NOTE: The Palette API can extract the following profiles from an image
+             1. Vibrant
+             2. Vibrant Dark / Light
+             3. Muted
+             4. Muted Dark / Light
+         */
+        Palette.generateAsync(photo, new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                int bgColor = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
+                holder.placeNameHolder.setBackgroundColor(bgColor);
+            }
+        });
     }
 }
