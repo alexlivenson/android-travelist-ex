2dc49045f98d7b2a5f500d36bb139fc67857ac3b
alex livenson <alexlivenson1@alexs-MacBook-Pro.local>
2016-02-08 22:42:09 -0800

    Added on click event

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -7,60 +7,66 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
-  private Menu menu;
-  private boolean isListView;
-  private RecyclerView mRecyclerView;
-  private StaggeredGridLayoutManager mStaggeredLayoutManager;
+    private Menu menu;
+    private boolean isListView;
+    private RecyclerView mRecyclerView;
+    private StaggeredGridLayoutManager mStaggeredLayoutManager;
+    private TravelListAdapter mAdapter;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    isListView = true;
-    mRecyclerView = (RecyclerView) findViewById(R.id.list);
-    // Count 1 for span count makes this a list rather then a grid
-    mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
-    mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
-  }
+        isListView = true;
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        // Count 1 for span count makes this a list rather then a grid
+        mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
 
-  private void setUpActionBar() {
-
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.menu_main, menu);
-    this.menu = menu;
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    int id = item.getItemId();
-    if (id == R.id.action_toggle) {
-      toggle();
-      return true;
+        mAdapter = new TravelListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
     }
-    return super.onOptionsItemSelected(item);
-  }
 
-  private void toggle() {
-    MenuItem item = menu.findItem(R.id.action_toggle);
-    if (isListView) {
-      item.setIcon(R.drawable.ic_action_list);
-      item.setTitle("Show as list");
-      isListView = false;
-    } else {
-      item.setIcon(R.drawable.ic_action_grid);
-      item.setTitle("Show as grid");
-      isListView = true;
+    private void setUpActionBar() {
+
     }
-  }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_toggle) {
+            toggle();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggle() {
+        MenuItem item = menu.findItem(R.id.action_toggle);
+        if (isListView) {
+            item.setIcon(R.drawable.ic_action_list);
+            item.setTitle("Show as list");
+            isListView = false;
+        } else {
+            item.setIcon(R.drawable.ic_action_grid);
+            item.setTitle("Show as grid");
+            isListView = true;
+        }
+    }
 }
---
+++ app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -0,0 +1,82 @@
+package bhouse.travellist_starterproject;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by alexlivenson1 on 2/8/16.
+ */
+// 1. Extend to override logic
+public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.ViewHolder> {
+    Context mContext;
+    OnItemClickListener mItemClickListener;
+
+    // 2. This is where context can be passed when create TravelListAdapter in MainActivity
+    public TravelListAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    // 3. View Holder class is Optional in ListView but required in RecyclerView (improves scrolling and performance by avoiding calling findViewById()
+    // NOTE: Usually always use RecyclerView instead of ListView
+    // NOTE: Unlike ListView, RecycleView does not come with an onClickListener
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public LinearLayout placeHolder;
+        public LinearLayout placeNameHolder;
+        public TextView placeName;
+        public ImageView placeImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            placeHolder = (LinearLayout) itemView.findViewById(R.id.mainHolder);
+            placeName = (TextView) itemView.findViewById(R.id.placeName);
+            placeNameHolder = (LinearLayout) itemView.findViewById(R.id.placeNameHolder);
+            placeImage = (ImageView) itemView.findViewById(R.id.placeImage);
+
+            placeHolder.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mItemClickListener != null) {
+                mItemClickListener.onItemClick(itemView, getLayoutPosition());
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+
+    // 1. Returns the number of items in the data array
+    @Override
+    public int getItemCount() {
+        return new PlaceData().placeList().size();
+    }
+
+    // 2. Returns new instance of view holder by passing in inflated view of row_places
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_places, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // 3. binds the Place object to the UI element in ViewHolder. You'll use Picasso to Cache those images
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final Place place = new PlaceData().placeList().get(position);
+        holder.placeName.setText(place.name);
+        Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+    }
+}
